8416675f00786cfc15eed706c3b878ee038e33d5
thebam <jason.j.saunders@gmail.com>
2016-01-27 15:26:08 -0500

    Fleshed out mongo db. Added ability to create a character and view list of existing characters

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,20 +1,46 @@
 var express = require('express');
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 var adminRouter = express.Router();
-var list = { list: ['Superman', 'Batman', 'Spider-man'] };
 
+var mongoURL = 'mongodb://localhost:27017/heroSmasher';
 
 adminRouter.route('/')
     .get(function (req, res) {
         res.render('admin-add-character', list);
     });
-   adminRouter.route('/add') 
-.post(function (req, res) {
-    var sname = req.body.charname;
-        res.send(sname);
+adminRouter.route('/add')
+    .post(function (req, res) {
+        var name = req.body.characterName;
+        var biography = req.body.biography;
+        var allPowers = req.body.allPowers;
+        var allTraits = req.body.allTraits;
+        var powers = JSON.parse(allPowers).powers;
+        var traits = JSON.parse(allTraits).traits;
+        var character = {
+            name: name,
+            powers: powers,
+            traits: traits,
+            bio: biography
+        };
+        
+        mongodb.connect(mongoURL, function (err, db) {
+            var collection = db.collection('characters');
+            collection.insert(character, function (err, results) {
+                res.send(results);
+                db.close();
+            });
+        });
     });
 adminRouter.route('/:id')
     .get(function(req,res){
-        var id = req.params.id;
-        res.render('character',{character:list.list[id]});
+        var id = new objectId(req.params.id);
+        mongodb.connect(mongoURL, function (err, db) {
+            var collection = db.collection('characters');
+            collection.findOne({ _id: id }, function (err, results) {
+                res.render('character', { character: results });
+                db.close();
+            });
+        });
     });
 module.exports = adminRouter;
--- src/routes/characterRoutes.js
+++ src/routes/characterRoutes.js
@@ -1,15 +1,30 @@
 var express = require('express');
+var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 var characterRouter = express.Router();
-var list = { list: ['Superman', 'Batman', 'Spider-man'] };
 
+var mongoURL = 'mongodb://localhost:27017/heroSmasher';
 
 characterRouter.route('/')
     .get(function (req, res) {
-        res.render('characters', list);
+        
+        mongodb.connect(mongoURL, function (err, db) {
+            var collection = db.collection('characters');
+            collection.find().toArray(function (err, results) {
+                res.render('characters', { characters: results });
+                db.close();
+            });
+        });
     });
 characterRouter.route('/:id')
-.get(function(req,res){
-    var id = req.params.id;
-    res.render('character',{character:list.list[id]});
-});
+    .get(function (req, res) {
+        var id = new objectId(req.params.id);
+        mongodb.connect(mongoURL, function (err, db) {
+            var collection = db.collection('characters');
+            collection.findOne({ _id: id }, function (err, results) {
+                res.render('character', { character: results });
+                db.close();
+            });
+        });
+    });
 module.exports = characterRouter;
